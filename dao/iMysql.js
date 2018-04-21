d857398914fb7794d169c52e0024a8c4e52e8626
jun266a <jun266a@foxmail.com>
2018-04-22 00:20:04 +0800

    rebuild sqlBase

--- dao/iMysql.js
+++ dao/iMysql.js
@@ -2,83 +2,71 @@
 //1.引入mysql的模块
 const mysql = require('mysql');
 
-
-const iDB = {
-	HOST : '127.0.0.1',
-	USER : 'root',
-	PASSWORD : '123',
-	DATABASE_NAME : 'kali',
+let db = {
+	iDB :  {
+		HOST : '127.0.0.1',
+		USER : 'root',
+		PASSWORD : '123',
+		DATABASE_NAME : 'kali',
+	},
+	TABLE : {
+		NAME : 'BLOG',
+		ID : 'UID',
+		EMAIL : 'EMAIL',
+		PASSWORD : 'PASSWORD',
+	},
+	query : function sqlback(querySql,values,callback){
+		//2.获取和数据库的链接
+		const connection = mysql.createConnection({
+			host : this.iDB.HOST,
+			user : this.iDB.USER,
+			password : this.iDB.PASSWORD,
+			database : this.iDB.DATABASE_NAME,
+		});
+		//3.启动链接
+		connection.connect(function(err){
+			if (err) {
+			    console.error('连接错误: ' + err.stack);
+			    return;
+		  	}
+		  	console.log('连接ID ' + connection.threadId);
+		});
+		if(!querySql) return ;
+		connection.query(querySql,values,function(err,results,fields){
+			if(err){
+	    		console.error('查询错误: ' + err.stack);
+			    return;
+		    }
+			callback(results);
+		});
+		connection.end(function(err) {
+			if(err){
+				console.error('关闭错误: ' + err.stack);
+			    return;
+		    }else{
+		    	console.log('连接关闭');
+		    }
+		});
+	},
 };
 
-const TABLE = {
-	NAME : 'BLOG',
-	ID : 'UID',
-	EMAIL : 'EMAIL',
-	PASSWORD : 'PASSWORD',
-};
-
-//2.获取和数据库的链接
-const connection = mysql.createConnection({
-	host : iDB.HOST,
-	user : iDB.USER,
-	password : iDB.PASSWORD,
-	database : iDB.DATABASE_NAME,
-});
-//3.启动链接
-connection.connect(function(err){
-	if (err) {
-	    console.error('连接错误: ' + err.stack);
-	    return;
-  	}
-  	console.log('连接ID ' + connection.threadId);
-});
 
 
-//4.执行增删查改
-//single & multiple select
-function select(values,callback){
-	let selectSql = 'SELECT * FROM ' + TABLE.NAME +' WHERE ?';
-	console.log(selectSql);
-	console.log(values);
-	
-	connection.query(selectSql,values,function(err,results){
-		if(err){
-    		console.error('查询错误: ' + err.stack);
-		    return;
-	    }
-		callback(results);
+(function test (){
+//	select test 
+//	let testJson = {};
+//	testJson.email = '11@11';
+//	select(testJson,function(results){
+//		console.log(results);
+//	});
+//  insert test
+	let testArray = [];
+	testArray.push("00@00");
+	testArray.push("00");
+	insert(testArray,function(results){
+		console.log(results);
 	});
-};
-//single insert
-function insert(values,callback){
-	let insertSql = 'INSERT INTO ' + TABLE.NAME +' ('+TABLE.EMAIL+','+TABLE.PASSWORD+')'+'  VALUES (?,?) ';
-	console.log(insertSql);
-	connection.query(insertSql,values,function(err,results){
-		if(err){
-    		console.error('插入错误: ' + err.stack);
-		    return;
-	    }
-		callback(results);
-	});
-};
+});
 
 
-let test = function(){
-//	let Json = {email:'1@66'};
-//	select(Json);
-
-//	let Json = ['1@66', '66'];
-//	insert(Json);
-	
-}();
-
-//connection.end(function(err) {
-//	if(err){
-//		console.error('关闭错误: ' + err.stack);
-//	    return;
-//  }
-//});
-
-module.exports = {
-	insert,select,
-}
+module.exports = db;
