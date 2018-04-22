076a8ab8b96b02bb96a7c66038e24a9150844b8e
jun266a <jun266a@foxmail.com>
2018-04-22 14:46:24 +0800

    update signin signup action

--- webapp/js/signin.js
+++ webapp/js/signin.js
@@ -3,24 +3,26 @@ $(function(){
 		//获取表单数据
 //      var datasArray = $('#signin-form').serializeArray();
         //datasArray instanceof Array true
-        var datasParam = $('#signin-form').serialize();
+        var Param = $('#signin-form').serialize();
         //param
-        
-        console.log(datasParam);
-        
-//      var jsonstr = paramToJson(datas);//吧键值对解析为json字符串
-//      console.log('jsonstr'+jsonstr);
-//      var json = strToJson(jsonstr);//吧json字符串解析为json对象
-//      console.log('json'+json);
-
-		//这里不用对参数进行转化，
-//		不论是对象数组或是对象JSON或者不转化，通过ajax 方式传递数据，，在后台接收的是参数化的
-        AJAX(datasParam,"/signinAction",function(json){
-        	if(json.desc === '1'){
-        		$(location).prop('href', '../../index.html')
-        		console.log(json.desc === '1');
-        	}
-        });
+        var Boolean = true;
+        var jsonStr = paramToJson(Param);//吧键值对解析为json字符串
+        var jsonObj = strToJson(jsonStr);//吧json字符串解析为json对象
+		if(!jsonObj.confirm.equals(jsonObj.password)){
+			return;
+		}
+		if(jsonObj.password=='' || jsonObj.email == ''){
+			Boolean = false;
+			return;
+		}
+		if(Boolean){
+			AJAX(Param,"/signinAction",function(json){
+				if(json.desc === '1'){
+					$(location).prop('href', '../../index.html')
+					console.log(json.desc === '1');
+				}
+			});
+		}
         
 	});
 });
--- webapp/js/signup.js
+++ webapp/js/signup.js
@@ -1,16 +1,26 @@
 $(function(){
 	$('#signup').on('click',function(){
-        var datasParam = $('#signup-form').serialize();
-        console.log(datasParam);
-		//这里不用对参数进行转化，
-//		不论是对象数组或是对象JSON或者不转化，通过ajax 方式传递数据，，在后台接收的是参数化的
-		AJAX(datasParam,"/signupAction",function(json){
-			if(json.desc === '1'){
-        		$(location).prop('href', '../../signin.html')
-        		console.log(json.desc === '1');
-        	}
-		});
-        
+        var Param = $('#signup-form').serialize();
+       	console.log(Param);
+        //param
+        var Boolean = true;
+        var jsonStr = paramToJson(Param);//吧键值对解析为json字符串
+        var jsonObj = strToJson(jsonStr);//吧json字符串解析为json对象
+        if(jsonObj.password=='' || jsonObj.email == ''){
+			Boolean = false;
+			return;
+		}
+        if(!jsonObj.confirm == jsonObj.password){
+			return;
+		}
+		if (Boolean) {
+			AJAX(Param,"/signupAction",function(json){
+				if(json.desc === '1'){
+	        		$(location).prop('href', '../../signin.html')
+	        		console.log(json.desc === '1');
+	        	}
+			});
+		}
 	});
 });
 
