39a2a6feb21d8c749ce58e6595de44216aaa0d67
jun266a <jun266a@foxmail.com>
2018-04-30 17:27:53 +0800

    引入中间件express/body-parser，移除quertstring&formidable模块

--- iServer.js
+++ iServer.js
@@ -4,45 +4,51 @@
 const http = require('http');
 //引入路径模块path
 const path = require('path');
-//引入模块querystring
-const querystring = require('querystring');
-const formidable = require('formidable');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
 //引入自己的模块
 
-const itools = require('./common/tools')
-const iuser = require('./control/userAccountControl')
+const itools = require('./common/tools');
+const iuser = require('./control/userAccountControl');
+const iBlog = require('./control/blogControl');
 
-//2、创建服务器    函数的参数req是发送给服务器的请求，res是服务器的相应
-let httpObj = http.createServer(function(req,res){
-    //编写url  其中index.html是要服务器读取的文件
-    let url = req.url=='/'?'index.html':req.url;
-    //开始文件读取，参数分别是完整的url 编码 和读取完毕执行的函数，注意前后台编码需要一致
-    let pathname = path.join(__dirname,'/webapp',url);
-    
-    if(url === "/favicon.ico" ){
-    	pathname = path.join(__dirname,'/webapp/images/56.png');
-    }else if(url === "/signinAction" ){
-    	if(req.method == 'POST'){
-    		let form = new formidable.IncomingForm();
-    		form.parse(req,function (err,fields,files){
-    			iuser.userSignin(fields,res);
-		    });
-        }
-    }else if(url === "/signupAction" ){
-    	if(req.method == 'POST'){
-    		let form = new formidable.IncomingForm();
-    		form.parse(req,function (err,fields,files){
-    			iuser.userSignup(fields,res);
-    		});
-    	}
-    }else{
-    	itools.loadFile(pathname,res);
-    }  
+//post参数注入
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+//静态webapp文件夹，方便请求webapp的所有文件
+app.use(express.static('webapp'));
+
+app.get('/favicon.ico',function(req,res){
+    let	pathname = path.join(__dirname,'/webapp/images/56.png');
+    itools.loadFile(pathname,res);
+});
+app.post('/signinAction',function(req,res){
+	if(!req.body){
+		return res.sendStatus(400);
+	}else{
+    	iuser.userSignin(req.body,res);
+	}
+});
+app.post('/signupAction',function(req,res){
+	if(!req.body){
+		return res.sendStatus(400);
+	}else{
+    	iuser.userSignup(req.body,res);
+	}
+});
+app.post('/publishAction',function(req,res){
+	if(!req.body){
+		return res.sendStatus(400);
+	}else{
+    	iBlog.blogInsert(req.body,res);
+	}
 });
 //监听端口
-httpObj.listen(80,function(err){
+app.listen(80,function(err){
 	if(err){
     	console.error('错误: ' + err.stack);
 	}
